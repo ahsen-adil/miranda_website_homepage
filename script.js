772a3f5d16d30dde62c0259420d19842d957a99e
ahsen adil <161212941+ahsen-adil@users.noreply.github.com>
2024-08-04 15:36:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var tl = gsap.timeline()
+
+tl.to("#page1", {
+    y:"100vh",
+    scale:0.6, 
+    duration:0
+})
+
+tl.to("#page1", {
+    y:"0vh", 
+    duration:1,
+    delay:1
+    
+})
+
+tl.to("#page1", {
+    rotate:360, 
+    scale:1,
+    y:"0vh",
+    duration:0.8
+    
+})
